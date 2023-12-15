fbe3a33e8582dfa1ba7924d8c089346a23538d29
mila <milavoronina1997@mail.ru>
2023-12-15 20:59:36 +0500

    fourth task done

--- src/main/java/org/example/jdk/homework/three/task4/Main.java
+++ src/main/java/org/example/jdk/homework/three/task4/Main.java
@@ -6,12 +6,12 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        String key1 = "Key 1";
+        String key1 = "key 1";
         Integer value1 = 1;
         Pair<String, Integer> pair1 = new Pair<>(key1, value1);
 
         Map<String, Integer> key2 = new HashMap<>();
-        key2.put("Key", 2);
+        key2.put("key 2", 2);
         Double value2 = 2.0;
         Pair<Map<String, Integer>, Double> pair2 = new Pair<>(key2, value2);
 
