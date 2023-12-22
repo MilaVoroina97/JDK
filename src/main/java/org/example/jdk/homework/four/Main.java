8a8f79b1c5ef65c7ce4df9cdbe21610f98165feb
mila <milavoronina1997@mail.ru>
2023-12-22 19:16:38 +0500

    create menu

--- src/main/java/org/example/jdk/homework/four/Main.java
+++ src/main/java/org/example/jdk/homework/four/Main.java
@@ -1,4 +1,8 @@
 package org.example.jdk.homework.four;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+    }
 }
--- src/main/java/org/example/jdk/homework/four/Menu.java
+++ src/main/java/org/example/jdk/homework/four/Menu.java
@@ -2,18 +2,18 @@ package org.example.jdk.homework.four;
 
 public class Menu {
 
-    EmployeeOperations actions = new EmployeeOperations();
-    private UserInterface cv = new UserInterface();
+    EmployeeOperations operations = new EmployeeOperations();
+    private UserInterface userInterface = new UserInterface();
 
     public void run() {
 
         while (true) {
-            switch (cv.inputAction()) {
-                case 1 -> actions.addEmployee();
-                case 2 -> actions.findEmployeeByExperience();
-                case 3 -> actions.findPhonesByName();
-                case 4 -> actions.findEmployeeByIDNumber();
-                case 5 -> actions.addPhone();
+            switch (userInterface.inputAction()) {
+                case 1 -> operations.addEmployee();
+                case 2 -> operations.findEmployeeByExperience();
+                case 3 -> operations.findPhonesByName();
+                case 4 -> operations.findEmployeeByIDNumber();
+                case 5 -> operations.addPhone();
                 case 0 -> System.exit(0);
             }
         }
